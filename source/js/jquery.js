89bf6859e5e1b82cc9ec8d7e52f00f890d7a288f
Vladimir <Sp1rit911@mail.ru>
2019-11-04 22:07:08 +0300

    Some fixes with navigation

--- source/js/jquery.js
+++ source/js/jquery.js
@@ -1,1 +1,16 @@
-// There will be jquery code;$(document).ready(function(){});
+jQuery(function () {
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html').animate({
+        scrollTop: $(hash).offset().top
+      }, 500, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+  $('.main-nav__item').on('click', function(){
+    $('.main-nav').removeClass('main-nav--opened').addClass('main-nav--closed');
+  });
+});
